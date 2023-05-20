b7bfa2dc750bb79722e1fe57d9064b19a6caa168
Serhii Vidernikov <sv54@mscloud.ua.es>
2023-05-20 12:22:48 +0200

    Get Seguimiento

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,49 @@ namespace MTISTeoriaCliente
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void GetSeguimiento_Click(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Realiza una petición GET a una URL específica
+                    //TODO: Cambiar la url a localhost
+                    //HttpResponseMessage response = await client.GetAsync("http://localhost:9095/seguimiento?oper=get&codigo=" + Codigo1.Text);
+                    HttpResponseMessage response = await client.GetAsync("http://192.168.1.122:9095/seguimiento?oper=get&codigo=" + Codigo1.Text);
+
+                    // Verifica si la petición fue exitosa (código de respuesta 200-299)
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Lee el contenido de la respuesta como una cadena de texto
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        if (responseBody.Contains(Codigo1.Text + " no es valido"))
+                        {
+                            Console.WriteLine("Respuesta: " + responseBody);
+                            Estado.Text = responseBody;
+                        }
+                        else
+                        {
+                            //Quitamos los corchetes
+                            responseBody = responseBody.Substring(1, responseBody.Length - 2);
+                            Console.WriteLine("Respuesta: " + responseBody);
+                            JObject json = JObject.Parse(responseBody);
+                            Console.WriteLine("Respuesta: " + json["estado"]);
+                            Estado.Text = (string)json["estado"];
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("La petición no fue exitosa. Código de respuesta: " + response.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error en la petición: " + ex.Message);
+                }
+            }
         }
     }
 }
