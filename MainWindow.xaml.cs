b4e2bb19c7e1a98796944ec7a490d422154f5b58
Víctor Hernández Justamante <vhj1@alu.ua.es>
2023-05-21 12:32:04 +0200

    Añadido flujo entregar envio

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -35,8 +35,8 @@ namespace MTISTeoriaCliente
                 {
                     // Realiza una petición GET a una URL específica
                     //TODO: Cambiar la url a localhost
-                    //HttpResponseMessage response = await client.GetAsync("http://localhost:9095/seguimiento?oper=get&codigo=" + Codigo1.Text);
-                    HttpResponseMessage response = await client.GetAsync("http://192.168.1.122:9095/seguimiento?oper=get&codigo=" + Codigo1.Text);
+                    HttpResponseMessage response = await client.GetAsync("http://localhost:9095/seguimiento?oper=get&codigo=" + Codigo1.Text);
+                    //HttpResponseMessage response = await client.GetAsync("http://192.168.1.122:9095/seguimiento?oper=get&codigo=" + Codigo1.Text);
 
                     // Verifica si la petición fue exitosa (código de respuesta 200-299)
                     if (response.IsSuccessStatusCode)
@@ -67,6 +67,81 @@ namespace MTISTeoriaCliente
                 }
                 catch (Exception ex)
                 {
+                    Console.WriteLine("Error en la petición: " + ex.Message);
+                }
+            }
+        }
+
+        private async void Asignar_y_enviar_envio_pendiente_Click(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Realiza una petición GET a una URL específica
+                    HttpResponseMessage response = await client.GetAsync("http://localhost:9094/entrega/clienteDisponible?idEnvio=" + Id_envio.Text);
+
+                    // Verifica si la petición fue exitosa (código de respuesta 200-299)
+                    
+                    // Lee el contenido de la respuesta como una cadena de texto
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    Response.Text = responseBody;
+                        
+                }
+                catch (Exception ex)
+                {
+                    Response.Text = "Error";
+                    Console.WriteLine("Error en la petición: " + ex.Message);
+                }
+            }
+        }
+
+        private async void Cliente_no_disponible_en_entrega_Click(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Realiza una petición GET a una URL específica
+                    HttpResponseMessage response = await client.GetAsync("http://localhost:9094/entrega/clienteNoDisponible?idEnvio=" + Id_envio.Text);
+
+                    // Verifica si la petición fue exitosa (código de respuesta 200-299)
+
+                    // Lee el contenido de la respuesta como una cadena de texto
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    Response.Text = responseBody;
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Text = "Error";
+                    Console.WriteLine("Error en la petición: " + ex.Message);
+                }
+            }
+        }
+
+        private async void Cliente_disponible_en_entrega_Click(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Realiza una petición GET a una URL específica
+                    HttpResponseMessage response = await client.GetAsync("http://localhost:9094/entrega/clienteDisponible?idEnvio=" + Id_envio.Text);
+
+                    // Verifica si la petición fue exitosa (código de respuesta 200-299)
+
+                    // Lee el contenido de la respuesta como una cadena de texto
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    Response.Text = responseBody;
+
+                }
+                catch (Exception ex)
+                {
+                    Response.Text = "Error";
                     Console.WriteLine("Error en la petición: " + ex.Message);
                 }
             }
