86555b758ecd30f78d3bef7119b3b095a0d45e21
Víctor Hernández Justamante <vhj1@alu.ua.es>
2023-05-21 13:01:33 +0200

    Arreglado bugs en flujo envio

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -79,7 +79,7 @@ namespace MTISTeoriaCliente
                 try
                 {
                     // Realiza una petición GET a una URL específica
-                    HttpResponseMessage response = await client.GetAsync("http://localhost:9094/entrega/clienteDisponible?idEnvio=" + Id_envio.Text);
+                    HttpResponseMessage response = await client.GetAsync("http://localhost:9094/envio?idEnvio=" + Id_envio.Text);
 
                     // Verifica si la petición fue exitosa (código de respuesta 200-299)
                     
@@ -104,7 +104,7 @@ namespace MTISTeoriaCliente
                 try
                 {
                     // Realiza una petición GET a una URL específica
-                    HttpResponseMessage response = await client.GetAsync("http://localhost:9094/entrega/clienteNoDisponible?idEnvio=" + Id_envio.Text);
+                    HttpResponseMessage response = await client.GetAsync(" http://localhost:9094/entrega/clienteNoDisponible?idEnvio=" + Id_envio.Text);
 
                     // Verifica si la petición fue exitosa (código de respuesta 200-299)
 
